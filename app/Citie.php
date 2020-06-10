705e86bf7e2d28a443d3ef12ad197bc43f2a1f22
blasrodriguez <blasrodriguez934@gmail.com>
2020-06-09 22:55:06 -0300

    referencia a la tabla

---
+++ app/Citie.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Citie extends Model
+{
+    protected $table = 'cities';
+    protected $primaryKey = 'city_id';
+}
---
+++ app/Companie.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Companie extends Model
+{
+    protected $table = 'companies';
+    protected $primaryKey = 'company_id';
+}
